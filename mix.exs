b38b44c59eabeb03f9bd8733c7efa4a7221d1ce3
jia.wang <wj2016@gmail.com>
2023-11-13 20:31:26 +0100

    Start application

--- mix.exs
+++ mix.exs
@@ -17,7 +17,8 @@ defmodule ExNet.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {ExNet.Application, []}
     ]
   end
 
